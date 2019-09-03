4ab37a751540051630315a7625bcd7988fde1d30
Larry Lauer <larry.lauer.3d@gmail.com>
2019-09-02 22:42:26 -0700

    added parse of answered list

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -8,17 +8,34 @@
 # print the result
 
 
+# import scapy.all as scapy
+#
+# def scan(ip):
+#     arp_request = scapy.ARP(pdst=ip)
+#     arp_request.show()
+#     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+#     broadcast.show()
+#     arp_request_broadcast = broadcast/arp_request
+#     print(arp_request_broadcast.summary())
+#     arp_request_broadcast.show()
+#
+#     # scapy.ls(scapy.ARP())
+#
+# scan("10.0.2.1/24")
+
 import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
 
-    # scapy.ls(scapy.ARP())
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("----------------------------------------------------------------------")
+
+
 
 scan("10.0.2.1/24")
